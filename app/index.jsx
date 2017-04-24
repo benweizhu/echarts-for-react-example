d4ccc0149376b578bcf03c1186774ea3b5992ee1
benweizhu <benweizhu@gmail.com>
2017-04-24 17:18:33 +0800

    init

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class EchartsForReactExample extends React.Component {
+  render() {
+    return (
+      <div>Hello</div>
+    );
+  }
+}
+
+ReactDOM.render(<EchartsForReactExample />, document.querySelector('#app'));
