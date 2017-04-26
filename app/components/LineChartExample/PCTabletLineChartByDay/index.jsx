124f9207254ba680579d45fd9f58e691c6e933e3
benweizhu <benweizhu@gmail.com>
2017-04-26 14:54:34 +0800

    PCTabletLineChartByDay by day

--- app/components/LineChartExample/PCTabletLineChartByDay/index.jsx
+++ app/components/LineChartExample/PCTabletLineChartByDay/index.jsx
@@ -31,7 +31,7 @@ class PCTabletLineChartByDay extends React.Component {
     ];
   };
 
-  getPCData = ()=> {
+  getSixHourPCData = ()=> {
     return [
       { time: '16/01/07 6:00', pv: 38 },
       { time: '16/01/07 12:00', pv: 50 },
@@ -47,10 +47,65 @@ class PCTabletLineChartByDay extends React.Component {
     ];
   };
 
+  getPCData = ()=> {
+    return [
+      { time: '16/01/07', pv: 38 },
+      { time: '16/01/08', pv: 50 },
+      { time: '16/01/09', pv: 10 },
+      { time: '16/01/10', pv: 20 },
+      { time: '16/01/11', pv: 60 },
+      { time: '16/01/12', pv: 30 },
+      { time: '16/01/13', pv: 50 },
+      { time: '16/01/14', pv: 90 },
+      { time: '16/01/15', pv: 70 },
+      { time: '16/01/16', pv: 30 },
+      { time: '16/01/17', pv: 10 },
+      { time: '16/01/18', pv: 10 },
+      { time: '16/01/19', pv: 10 },
+      { time: '16/01/20', pv: 10 },
+      { time: '16/01/21', pv: 20 },
+      { time: '16/01/22', pv: 60 },
+      { time: '16/01/23', pv: 30 },
+      { time: '16/01/24', pv: 50 },
+      { time: '16/01/25', pv: 90 },
+      { time: '16/01/26', pv: 70 },
+      { time: '16/01/27', pv: 30 },
+    ];
+  };
+
+  getData =()=>{
+    return [
+      { time: '16/01/07', pv: 380 },
+      { time: '16/01/08', pv: 500 },
+      { time: '16/01/09', pv: 100 },
+      { time: '16/01/10', pv: 200 },
+      { time: '16/01/11', pv: 600 },
+      { time: '16/01/12', pv: 300 },
+      { time: '16/01/13', pv: 500 },
+      { time: '16/01/14', pv: 900 },
+      { time: '16/01/15', pv: 700 },
+      { time: '16/01/16', pv: 300 },
+      { time: '16/01/17', pv: 100 },
+      { time: '16/01/18', pv: 100 },
+      { time: '16/01/19', pv: 380 },
+      { time: '16/01/20', pv: 500 },
+      { time: '16/01/20', pv: 100 },
+      { time: '16/01/21', pv: 200 },
+      { time: '16/01/22', pv: 600 },
+      { time: '16/01/23', pv: 300 },
+      { time: '16/01/24', pv: 500 },
+      { time: '16/01/25', pv: 900 },
+      { time: '16/01/26', pv: 700 },
+      { time: '16/01/27', pv: 300 },
+    ];
+  }
+
+
   render() {
     return (
       <div className={cx('container')}>
-        <PCTabletLineChart bySixHours pcData={this.getPCData()} tabletData={this.getSexHourData()}/>
+        <PCTabletLineChart bySixHours pcData={this.getSixHourPCData()} tabletData={this.getSexHourData()}/>
+        <PCTabletLineChart pcData={this.getPCData()} tabletData={this.getData()}/>
       </div>
     );
   }
